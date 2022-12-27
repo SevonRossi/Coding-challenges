3d602cfad4c4565b81a78cf19fa9c80ad253a0cc
Srossi28 <105078485+Srossi27@users.noreply.github.com>
2022-12-27 09:04:06 -0600

    Add project files.

---
+++ Coding-challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Codingchallenge 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Input number: ");
+        long num = long.Parse(Console.ReadLine());
+
+        for (long i = 2; i < num; i++)
+        {
+            bool isPrime = true;
+            
+            for (long j= 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        }
+    }
+}
